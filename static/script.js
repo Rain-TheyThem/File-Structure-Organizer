dbbccdfac11875d5a7e145ca2a93265356f22cfc
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-09-18 19:15:07 -0400

    Add label for filename input

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
 function addFile() {
-    file_str = "<div class='file'><input type='text' id='file' placeholder='filename.txt'></div>"
+    file_str = "<div class='file'><lable for='file'>Filename: </label><input type='text' id='file' placeholder='filename.txt'></div>"
     document.getElementById("items").innerHTML += file_str
 }
