fc404eded9698144730b2bc27771d715269039e8
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-09-18 20:14:52 -0400

    Add `addFolder()` function for adding a folder

--- static/script.js
+++ static/script.js
@@ -2,3 +2,15 @@ function addFile() {
     file_str = "<div class='file'><label for='file'>Filename: </label><input type='text' id='file' placeholder='filename.txt'></div>"
     document.getElementById("items").innerHTML += file_str
 }
+
+function addFolder() {
+    let folderStr = `
+    <div class="folder">
+        <div class="folder-name">
+            <label for="folder-name">Folder Name: </label>
+            <input type="text" id="folder-name" placeholder="Folder Name">
+        </div>
+        <div id="folder-contents"></div>
+    </div>`
+    document.getElementById("items").innerHTML += folderStr
+}
